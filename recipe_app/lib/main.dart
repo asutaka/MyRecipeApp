23ede1a5489f896fdd445c0035c2f9fdefbb7dc3
Nguyễn Văn Phú <phunv@ftech.ai>
2023-10-31 17:53:22 +0700

    merge 2 project

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'app.dart';
+import 'di/service_locator.dart';
 
-void main() {
+// void main() {
+//   runApp(MyApp());
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setPreferredOrientations();
+  await ServiceLocator.configureDependencies();
   runApp(MyApp());
+}
+
+Future<void> setPreferredOrientations() {
+  return SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
 }
